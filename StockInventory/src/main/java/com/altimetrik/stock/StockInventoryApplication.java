d0ac54e83b1673493b62c93e85b9a3a882efc57e
DELL <DELL@DESKTOP-5FE0N5F>
2020-09-04 16:55:53 +0530

    package corrected

--- StockInventory/src/main/java/com/altimetrik/stock/StockInventoryApplication.java
+++ StockInventory/src/main/java/com/altimetrik/stock/StockInventoryApplication.java
@@ -1,4 +1,4 @@
-package com.altimetrik.stock.StockInventory;
+package com.altimetrik.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
